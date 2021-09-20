d027f233e3ffd9031f67d9bf7c4463b8ace5ba8c
Kittipong Nasoongnern <kittipongga@gmail.com>
2021-09-20 23:11:25 +0700

    update typeDefs

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
+import fs from "fs";
+import path from "path";
 import { ApolloServer } from "apollo-server-express";
-import typeDefs from "./schema/typeDefs";
-import resolvers from './resolvers/resolvers'
+
+import resolvers from "./resolvers/resolvers";
+
+const typeDefs = fs
+  .readFileSync(path.join(__dirname, "./schema", "schema.graphql"), "utf8")
+  .toString();
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-  });
+  typeDefs,
+  resolvers,
+});
 
 export default server;
